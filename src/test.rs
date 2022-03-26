5831879cb2cbd3a9c60662704beb8af8d32178fa
Defmc <defmc@github.com>
2022-03-26 19:17:15 -0400

    fix: remove binary entry point, use tests - library compliant

---
+++ src/test.rs
@@ -0,0 +1,25 @@
+use std::thread;
+use crate::{ok, info, warn, error, self as plog};
+
+#[test]
+fn multithread() {
+    info!("Application started");
+    let thrds: Vec<_> = (0..=1000)
+        .map(|x| thread::spawn(move || {
+            ok!("{x}th thread initialized");
+            ok!("{x}th thread finalized");
+        }))
+        .collect();
+
+    thrds.into_iter()
+        .for_each(|thrd| assert!(thrd.join().is_ok()));
+}
+
+#[test]
+fn pretty_out() {
+    let n = 5;
+    ok!("it's working {n}");
+    info!("it's started to work {n}");
+    warn!("wait, is it really working {n}?");
+    error!("OHHHH NOOOO");
+}
