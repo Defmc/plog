559801ef93450c7af80e4972750f0ca993b8a9cd
Defmc <defmc@github.com>
2022-03-27 14:26:37 -0400

    fix: split feature macros to external crates don't assume `false` on `cfg` checks

--- src/macros.rs
+++ src/macros.rs
@@ -23,61 +23,77 @@ pub fn datetime(_input: &mut String) {
 #[cfg(feature = "context")]
 #[macro_export]
 macro_rules! context {
-    ($input:tt) => {{
+    ($input:tt) => {
         $input.push_str(&format!(" at {}:{}", file!(), line!()))
-    }}
+    }
 }
 
 #[cfg(not(feature = "context"))]
 #[macro_export]
 macro_rules! context {
-    ($input:tt) => {{
-        $input.push_str(&format!(" at {}:{}", file!(), line!()))
-    }}
+    ($input:tt) => {
+    }
 }
 
 #[macro_export]
-macro_rules! core_log {
+macro_rules! log {
     ($color:tt, $prefix:tt, $($args:tt)+) => {{
         let mut prefix = String::from($prefix);
         plog::macros::datetime(&mut prefix);
         plog::context!(prefix);
-        plog::log(#[cfg(feature = "colored")] crossterm::style::Color::$color, &prefix, format!($($args)+))
-            .expect(plog::macros::ERROR_LOG)
+        plog::core_log!($color, { prefix.as_ref() }, $($args)+).expect(plog::macros::ERROR_LOG)
     }}
 }
+
+#[cfg(feature = "colored")]
+#[macro_export]
+macro_rules! core_log {
+    ($color:tt, $prefix:expr, $($args:tt)+) => {
+        plog::log(crossterm::style::Color::$color, $prefix, format!($($args)+))
+    }
+}
+
+#[cfg(not(feature = "colored"))]
+#[macro_export]
+macro_rules! core_log {
+    ($color:tt, $prefix:tt, $($args:tt)+) => {
+        plog::log($prefix, format!($($args)+))
+    }
+}
+
+
 
 #[macro_export]
 macro_rules! debug {
     ($($args:tt)+) => {{
-        plog::core_log!(Grey, "DEBG", $($args)+)
+        plog::log!(Grey, "DEBG", $($args)+)
     }}
 }
 
 #[macro_export]
 macro_rules! info {
     ($($args:tt)+) => {{
-        plog::core_log!(White, "INFO", $($args)+)
+        plog::log!(White, "INFO", $($args)+)
     }}
 }
 
 #[macro_export]
 macro_rules! warn {
     ($($args:tt)+) => {{
-        plog::core_log!(Yellow, "WARN", $($args)+)
+        plog::log!(Yellow, "WARN", $($args)+)
     }}
 }
 
 #[macro_export]
 macro_rules! error {
     ($($args:tt)+) => {{
-        plog::core_log!(Red, "ERRO", $($args)+)
+        plog::log!(Red, "ERRO", $($args)+)
     }}
 }
 
 #[macro_export]
 macro_rules! ok {
     ($($args:tt)+) => {
-        plog::core_log!(Green, "OKAY", $($args)+)
+        plog::log!(Green, "OKAY", $($args)+)
     }
 }
