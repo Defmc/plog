41df7a819505d806c33cfdc6492966d677d7782a
Defmc <defmc@github.com>
2022-04-02 10:15:20 -0400

    doc: include documentation for `persistent` feature

--- src/persistent.rs
+++ src/persistent.rs
@@ -1,3 +1,6 @@
+/// Persistent log mode, enabled by `persistent` feature
+/// Writes to a file (defined with `LOG_FILEPATH` environment variable)
+/// Thread-safe, requires `parking_lot` to compile
 use parking_lot::{const_mutex, Mutex};
 use std::env;
 use std::error::Error;
@@ -5,10 +8,17 @@ use std::fs::{File, OpenOptions};
 use std::io::{self, Write};
 use std::sync::atomic::{AtomicBool, Ordering};
 
+/// Output file, defined in the first `plog::log` call
 static LOG_FILE: Mutex<Option<File>> = const_mutex(None);
+
+/// A `LOG_FILE.is_some()` wrapper, used to not freeze the `LOG_FILE`
 static USE_LOG_FILE: AtomicBool = AtomicBool::new(false);
+
+/// Used to define if the environment was already checked
 static CHECKED_ENV: AtomicBool = AtomicBool::new(false);
 
+/// Checks the environment
+/// If a `LOG_FILE` was defined, will create a file and set `USE_LOG_FILE` as true
 pub fn check_env() -> bool {
     if !CHECKED_ENV.load(Ordering::SeqCst) {
         CHECKED_ENV.store(true, Ordering::SeqCst);
@@ -17,6 +27,7 @@ pub fn check_env() -> bool {
     USE_LOG_FILE.load(Ordering::Acquire)
 }
 
+/// Creates the log file. If already exists something with the same filepath, just append it
 fn init_log_file() -> Result<(), Box<dyn Error>> {
     let path = env::var("LOG_FILEPATH")?;
     let file = OpenOptions::new()
