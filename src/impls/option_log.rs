9b80bb738b0e2591547715877253947ed760c006
Defmc <defmc@github.com>
2022-04-02 10:14:24 -0400

    doc: include documentation for `impls`

--- src/impls/option_log.rs
+++ src/impls/option_log.rs
@@ -1,14 +1,36 @@
+//! Plog implementation for Option
+//! defines some methods to work with `plog` using `std::option::Option` wrapper
+//! Due to adhension to method chains, every method consumes and return the ownership
+//! like `Option::map`, `Option::and_then`, etc.
+
 use crate as plog;
 use crate::{info, ok, warn};
 use std::fmt::{Debug, Display};
 
+/// Methods to work with. Where `log = show_none + show_some`
 pub trait OptionLog<T> {
+    /// show the content of a `Option` in a `plog::ok!` log or
+    /// a "{name} is empty" message in `plog::warn!` when it's None
     fn log(self, _: T) -> Self;
+
+    /// just show when `Option::is_none` return true
     fn show_none(self, _: T) -> Self;
+
+    /// just show when `Option::is_some` returns true
     fn show_some(self, _: T) -> Self;
 }
 
 impl<T: Debug, U: Display> OptionLog<U> for Option<T> {
+    /// Default implementation. Shows "{name} has {item:?}" for `Option::Some`
+    /// and "{name} is empty" for `Option::None`
+    /// ```rust
+    /// use plog::impls::*;
+    ///
+    /// let opt_none: Option<&str> = None;
+    /// let opt_hello = Some("Hello world");
+    /// opt_none.log("none"); // Logs "[WARN]: none is empty"
+    /// opt_hello.log("hello"); // Logs "[OKAY]: hello has "Hello world""
+    /// ```
     fn log(self, name: U) -> Self {
         match self {
             Some(ref x) => ok!("{name} has {x:?}"),
@@ -17,6 +39,13 @@ impl<T: Debug, U: Display> OptionLog<U> for Option<T> {
         self
     }
 
+    /// Like `OptionLog::log`, but just works for `Option::None`
+    /// ```rust
+    /// use plog::impls::*;
+    ///
+    /// let opt_none: Option<&str> = None;
+    /// opt_none.log("none"); // Logs "[WARN]: none is empty"
+    /// ```
     fn show_none(self, name: U) -> Self {
         if let None = self {
             info!("{name} is empty");
@@ -24,6 +53,13 @@ impl<T: Debug, U: Display> OptionLog<U> for Option<T> {
         self
     }
 
+    /// Like `OptionLog::log`, but just works for `Option::Some`
+    /// ```rust
+    /// use plog::impls::*;
+    ///
+    /// let opt_hello = Some("Hello world");
+    /// opt_hello.log("hello"); // Logs "[OKAY]: hello has "Hello world""
+    /// ```
     fn show_some(self, name: U) -> Self {
         if let Some(ref x) = self {
             info!("{name} has {x:?}");
--- src/impls/result_log.rs
+++ src/impls/result_log.rs
@@ -1,9 +1,21 @@
+//! Plog implementation for Result
+//! defines some methods to work with `plog` using `std::result::Result` wrapper
+//! Due to adhension to method chains, every method consumes and return the ownership
+//! like `Result::map`, `Result::inspect`, etc.
+
 use crate::{self as plog, error, ok};
 use std::fmt::{Debug, Display};
 
+/// Methods to work with. Where `log = show_ok + show_err`
 pub trait ResultLog<T> {
+    /// show the content of a `Option` in a `plog::ok!` log or
+    /// a "{name} is empty" message in `plog::warn!` when it's None
     fn log(self, _: T) -> Self;
+
+    /// just show when `Option::is_none` return true
     fn show_ok(self, _: T) -> Self;
+
+    /// just show when `Option::is_some` returns true
     fn show_err(self, _: T) -> Self;
 }
 
@@ -13,6 +25,16 @@ where
     U: Debug,
     N: Display,
 {
+    /// Default implementation. Shows "{name} has {item:?}" for `Option::Some`
+    /// and "{name} is empty" for `Option::None`
+    /// ```rust
+    /// use plog::impls::*;
+    ///
+    /// let opt_none: Option<&str> = None;
+    /// let opt_hello = Some("Hello world");
+    /// opt_none.log("none"); // Logs "[WARN]: none is empty"
+    /// opt_hello.log("hello"); // Logs "[OKAY]: hello has "Hello world""
+    /// ```
     fn log(self, name: N) -> Self {
         match self {
             Ok(ref val) => ok!("{name} returned {val:?}"),
@@ -21,6 +43,13 @@ where
         self
     }
 
+    /// Like `OptionLog::log`, but just works for `Option::None`
+    /// ```rust
+    /// use plog::impls::*;
+    ///
+    /// let opt_none: Option<&str> = None;
+    /// opt_none.log("none"); // Logs "[WARN]: none is empty"
+    /// ```
     fn show_err(self, name: N) -> Self {
         if let Err(ref err) = self {
             error!("{name} was failed with {err:?}");
@@ -28,6 +57,13 @@ where
         self
     }
 
+    /// Like `OptionLog::log`, but just works for `Option::Some`
+    /// ```rust
+    /// use plog::impls::*;
+    ///
+    /// let opt_hello = Some("Hello world");
+    /// opt_hello.log("hello"); // Logs "[OKAY]: hello has "Hello world""
+    /// ```
     fn show_ok(self, name: N) -> Self {
         if let Ok(ref val) = self {
             ok!("{name} was failed with {val:?}");
