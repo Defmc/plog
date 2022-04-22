b306be39ab0a9147137d224096aef715985581e4
Defmc <defmc0101@gmail.com>
2022-04-22 08:32:09 -0400

    rev(`OptionLog`): remove useless `debug` methods - since, disable it as a feature is better

--- src/impls/option_log.rs
+++ src/impls/option_log.rs
@@ -10,22 +10,13 @@ use std::fmt::{Debug, Display};
 /// Methods to work with. Where `log = show_none + show_some`
 pub trait OptionLog<T> {
     /// Show the content of a `Option` in a `plog::ok!` log or
-    /// a "{name} is empty" message in `plog::warn!` when it's None
+    /// a "{name} is empty" message in `plog::warn!` when it's `Option::None`
     fn log(self, _: T) -> Self;
 
     /// Just shows when it's `Option::is_none` return true
     fn show_none(self, _: T) -> Self;
 
     /// Just shows when it's `Option::is_some` returns true
-    fn show_some(self, _: T) -> Self;
-
-    /// Debug log variant.
-    fn debug(self, _: T) -> Self;
-    
-    /// Debug `show_none` variant.
-    fn show_none(self, _: T) -> Self;
-
-    /// Debug `show_some` variant.
     fn show_some(self, _: T) -> Self;
 }
 
@@ -76,47 +67,6 @@ impl<T: Debug, U: Display> OptionLog<U> for Option<T> {
         if let Some(ref x) = self {
             info!("{_name} has {x:?}");
         }
-        self
-    }
-
-    /// Like `OptionLog::log`, but just shows on debug builds
-    /// ```rust
-    /// use plog::impls::*;
-    ///
-    /// let opt_none: Option<&str> = None;
-    /// let opt_hello = Some("Hello world");
-    /// opt_none.debug("none"); // Just logs "[WARN]: none is empty" on debug builds
-    /// opt_hello.debug("hello"); // Just logs "[OKAY]: hello has "Hello world"" on debug builds
-    /// ```
-    fn debug(self, _name: U) -> Self {
-        #[cfg(debug_assertions)]
-        let self = self.debug(_name);
-        self
-    }
-
-     /// Like `OptionLog::show_none`, but just shows on debug builds
-    /// ```rust
-    /// use plog::impls::*;
-    ///
-    /// let opt_none: Option<&str> = None;
-    /// opt_none.log("none"); // Logs "[WARN]: none is empty"
-    /// ```
-    fn show_none(self, _name: U) -> Self {
-        #[cfg(debug_assertions)]
-        let self = self.show_none(_name);
-        self
-    }
-
-    /// Like `OptionLog::show_some`, but just shows on debug builds
-    /// ```rust
-    /// use plog::impls::*;
-    ///
-    /// let opt_hello = Some("Hello world");
-    /// opt_hello.log("hello"); // Logs "[OKAY]: hello has "Hello world""
-    /// ```
-    fn debug_some(self, _name: U) -> Self {
-        #[cfg(debug_assertions)]
-        let self = self.show_some(_name);
         self
     }
 }
